4b9dbb4a077077ddf8e8264e2a343ce1bedc8ba0
Leather128 <81660872+Leather128@users.noreply.github.com>
2022-02-15 16:00:19 -0500

    when more optimized code:

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,54 +3,61 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
-import flixel.math.FlxMath;
 import flixel.text.FlxText;
-import flixel.ui.FlxButton;
 import flixel.util.FlxColor;
-import openfl.display.Sprite;
 
 class PlayState extends FlxState
 {
 	var text:FlxText;
 	var sprite:FlxSprite;
+	var grid:FlxSprite;
 
 	override public function create()
 	{
-		// I looked up image on google and this was the first result.
-		sprite = new FlxSprite();
-		sprite.makeGraphic(300, 300, flixel.util.FlxColor.WHITE);
-		sprite.loadGraphic(AssetPaths.Image__png);
-		sprite.x = 100;
-		sprite.y = 0;
 		super.create();
+
+		sprite = new FlxSprite(100, 0);
+		sprite.loadGraphic(AssetPaths.Image__png);
+
+		grid = new FlxSprite(100, 0);
+		grid.makeGraphic(300, 300, flixel.util.FlxColor.WHITE);
+		grid.pixelPerfectRender = true;
+
+		for (y in 0...Std.int(grid.width))
+		{
+			for (x in 0...Std.int(grid.height))
+			{
+				if (x % 2 == 1 && y % 2 == 1)
+					grid.pixels.setPixel(x, y, 0x0000ff);
+				if (x < 5 || y < 5 || x > 295 || y > 295)
+					grid.pixels.setPixel(x, y, 0xffffff);
+			}
+		}
+
 		text = new FlxText(0, 0, FlxG.width, "Hello World", 64);
 		text.setFormat(null, 64, FlxColor.RED, FlxTextAlign.CENTER);
+
 		add(text);
+		add(sprite);
+		add(grid);
 	}
 
 	override public function update(elapsed:Float):Void
 	{
 		super.update(elapsed);
+
 		text.y++;
+		sprite.x++;
+		grid.x++;
+
 		if (text.y > FlxG.height)
 			text.y = 0 - 64;
-		for (y in 0...300)
-		{
-			{
-				for (x in 0...300)
-				{
-					if (x % 2 == 1 && y % 2 == 1)
-						sprite.pixels.setPixel(x, y, 0x0000ff);
-					if (x < 5 || y < 5 || x > 295 || y > 295)
-						sprite.pixels.setPixel(x, y, 0xffffff);
-				}
-			}
-		}
-		sprite.x += 1;
+
 		if (sprite.x > FlxG.height)
 			sprite.x = 0 - 64;
 
-		add(sprite);
+		if (grid.x > FlxG.height)
+			grid.x = 0 - 64;
 	}
 
 	function myCallback():Void {}
